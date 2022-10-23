99850606e68ac4e82d68de5b6e320d2dae8c7ed4
Chahyon Ku <chahyon.ku@gmail.com>
2022-10-22 23:00:45 -0500

    simpler hrnet_model

--- lib/pose_model.py
+++ lib/pose_model.py
@@ -7,13 +7,9 @@ class PoseModel(torch.nn.Module):
         super(PoseModel, self).__init__()
 
         self.add_module('hrnet', hrnet)
-        self.pool = torch.nn.AdaptiveAvgPool2d((128, 128))
-        self.final = torch.nn.Conv2d(1984, 17, 1, 1, 0)
+        self.final = torch.nn.Conv2d(64, 17, 1, 1, 0)
 
     def forward(self, image):
         out = self.get_submodule('hrnet')(image)
-        for i, branch in enumerate(out):
-            out[i] = self.pool(out[i])
-        out = torch.cat(out, dim=1)
-        out = self.final(out)
+        out = self.final(out[0])
         return out
