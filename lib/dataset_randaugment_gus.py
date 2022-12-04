a8e408c00fb85a0cbf1eccdbac318aeee8bddb92
rpm <chahyon.ku@gmail.com>
2022-12-03 18:10:11 -0600

    no error. shifted heatmap

--- lib/dataset_randaugment_gus.py
+++ lib/dataset_randaugment_gus.py
@@ -5,6 +5,7 @@ import cv2
 import torch.utils.data
 import torchvision
 import torchvision.transforms.functional as F
+import math
 
 def _apply_op2(img, op_name, magnitude, interpolation, fill=None):
     if op_name == "ShearX":
@@ -118,6 +119,39 @@ class OMCDataset(torch.utils.data.Dataset):
         image = cv2.imdecode(self.h5f[key]['image'][()], cv2.IMREAD_COLOR)
         image = torch.from_numpy(image)
         image = torch.permute(image, (2, 0, 1))
+
+        image = F.resize(image, [box_h * self.image_size // box_s, box_w * self.image_size // box_s])
+        pad_x = (self.image_size - image.shape[2]) // 2
+        pad_y = (self.image_size - image.shape[1]) // 2
+        image = F.pad(image, [pad_x, pad_y,
+                              self.image_size - image.shape[2] - pad_x,
+                              self.image_size - image.shape[1] - pad_y])
+
+        # get landmarks before any changes made
+        target = torch.zeros(17, self.target_size, self.target_size, dtype=torch.float32)
+        if len(data['landmarks']):
+            target = torch.zeros(17, self.target_size, self.target_size, dtype=torch.float32)
+            landmarks = numpy.array(data['landmarks'], dtype=int)
+            landmarks = numpy.stack((landmarks[0:len(landmarks):2], landmarks[1:len(landmarks):2]), axis=-1)
+
+        if len(data['landmarks']):
+            # landmarks already assigned
+            for i, (x, y) in enumerate(landmarks):
+                x = (x - box_x) * self.target_size // box_s + pad_x * self.target_size // self.image_size
+                y = (y - box_y) * self.target_size // box_s + pad_y * self.target_size // self.image_size
+                l = max(x - self.g.shape[0] // 2, 0)
+                t = max(y - self.g.shape[0] // 2, 0)
+                r = min(x + self.g.shape[0] // 2 + 1, self.target_size)
+                b = min(y + self.g.shape[0] // 2 + 1, self.target_size)
+                w = r - l
+                h = b - t
+                g_l = l - x + self.g.shape[0] // 2
+                g_t = t - y + self.g.shape[0] // 2
+                g_r = g_l + w
+                g_b = g_t + h
+                target[i, t:b, l:r] = self.g[g_t:g_b, g_l:g_r]
+        else:
+            print(data)
         
         # https://pytorch.org/vision/main/_modules/torchvision/transforms/autoaugment.html#RandAugment
         # Number of different magnitudes available to choose from
@@ -140,13 +174,6 @@ class OMCDataset(torch.utils.data.Dataset):
             "Equalize": (torch.tensor(0.0), False),
         }
 
-        # get landmarks before any changes made
-        target = torch.zeros(17, self.target_size, self.target_size, dtype=torch.float32)
-        if len(data['landmarks']):
-            target = torch.zeros(17, self.target_size, self.target_size, dtype=torch.float32)
-            landmarks = numpy.array(data['landmarks'], dtype=int)
-            landmarks = numpy.stack((landmarks[0:len(landmarks):2], landmarks[1:len(landmarks):2]), axis=-1)
-
         num_ops = 3
         magnitude_index = 5
         for _ in range(num_ops):
@@ -159,39 +186,13 @@ class OMCDataset(torch.utils.data.Dataset):
             image = torchvision.transforms.autoaugment._apply_op(image, op_name, magnitude_value, interpolation=torchvision.transforms.InterpolationMode.BILINEAR, fill=None)
             # apply_op2 across last two dimensions of target (operation for the underlying heat maps)
             # might be able to send all 17 channels though
-            target = _apply_op2(x, op_name, magnitude_value, interpolation=torchvision.transforms.InterpolationMode.BILINEAR, fill=None)
+            target = _apply_op2(target, op_name, magnitude_value, interpolation=torchvision.transforms.InterpolationMode.BILINEAR, fill=None)
+            
+        image = image.to(torch.get_default_dtype()).div(255)
+        image = F.normalize(image, self.mean, self.std)
         
         # Suggestion: check for landmarks earlier (only should be applied to training data)
 
-        image = image.to(torch.get_default_dtype()).div(255)
-        image = F.resize(image, [box_h * self.image_size // box_s, box_w * self.image_size // box_s])
-        image = F.normalize(image, self.mean, self.std)
-        pad_x = (self.image_size - image.shape[2]) // 2
-        pad_y = (self.image_size - image.shape[1]) // 2
-        image = F.pad(image, [pad_x, pad_y,
-                              self.image_size - image.shape[2] - pad_x,
-                              self.image_size - image.shape[1] - pad_y])
-
         metadata = torch.from_numpy(numpy.array([box_x, box_y, box_w, box_h, pad_x, pad_y]))
 
-        if len(data['landmarks']):
-            # landmarks already assigned
-            for i, (x, y) in enumerate(landmarks):
-                x = (x - box_x) * self.target_size // box_s + pad_x * self.target_size // self.image_size
-                y = (y - box_y) * self.target_size // box_s + pad_y * self.target_size // self.image_size
-                l = max(x - self.g.shape[0] // 2, 0)
-                t = max(y - self.g.shape[0] // 2, 0)
-                r = min(x + self.g.shape[0] // 2 + 1, self.target_size)
-                b = min(y + self.g.shape[0] // 2 + 1, self.target_size)
-                w = r - l
-                h = b - t
-                g_l = l - x + self.g.shape[0] // 2
-                g_t = t - y + self.g.shape[0] // 2
-                g_r = g_l + w
-                g_b = g_t + h
-                target[i, t:b, l:r] = self.g[g_t:g_b, g_l:g_r]
-        else:
-            print(data)
-
         return image, target, metadata
-
