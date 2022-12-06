20013ae56a46d7b54b69f1fc767347de64d4debf
rpm <chahyon.ku@gmail.com>
2022-12-05 20:02:10 -0600

    randaugment

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import argparse
 import collections
+import json
 import os
 import time
 
@@ -19,12 +20,12 @@ if __name__ == '__main__':
     # data
     parser.add_argument('--train_h5_path', type=str, default='data/v2/train.h5')
     parser.add_argument('--val_h5_path', type=str, default='data/v2/val.h5')
-    parser.add_argument('--n_workers', type=int, default=0)
-    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--n_workers', type=int, default=16)
+    parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--image_size', type=int, default=224)
-    parser.add_argument('--target_size', type=int, default=112)
+    parser.add_argument('--sigma', type=int, default=16)
     # model
-    parser.add_argument('--model_name', type=str, default='hrnet_w18',
+    parser.add_argument('--model_name', type=str, default='vit_base_patch16_224',
                         choices=('hrnet_w18', 'hrnet_w32', 'hrnet_w48', 'hrnet_w64',
                                  'vit_small_patch8_224_dino', 'vit_base_patch8_224_dino', 'vit_small_patch16_224_dino',
                                  'vit_base_patch16_224_dino',
@@ -36,21 +37,24 @@ if __name__ == '__main__':
     parser.add_argument('--lr', type=float, default=1e-3)
 
     # train
+    parser.add_argument('--device', type=str, default='cuda:0', choices=('cpu', 'cuda:0', 'cuda:1'))
     parser.add_argument('--f_save', type=int, default=999)
     parser.add_argument('--f_val', type=int, default=1)
-    parser.add_argument('--n_epochs', type=int, default=2)
-    parser.add_argument('--log_dir', type=str, default='logs/hrnet_w18')
+    parser.add_argument('--n_epochs', type=int, default=40)
+    parser.add_argument('--log_dir', type=str, default='logs/base/vit_base_patch16_224_16')
     args = parser.parse_args()
 
+    args.target_size = args.image_size // 4 if 'patch16' in args.model_name else args.image_size // 2
+
     # data
-    train_dataset = lib.dataset.OMCDataset(args.train_h5_path, args.image_size, args.target_size)
+    train_dataset = lib.dataset.OMCDataset(args.train_h5_path, args.image_size, args.target_size, args.sigma)
     train_dataloader = torch.utils.data.DataLoader(train_dataset, args.batch_size, shuffle=True, num_workers=args.n_workers)
-    val_dataset = lib.dataset.OMCDataset(args.val_h5_path, args.image_size, args.target_size)
+    val_dataset = lib.dataset.OMCDataset(args.val_h5_path, args.image_size, args.target_size, args.sigma)
     val_dataloader = torch.utils.data.DataLoader(val_dataset, args.batch_size, num_workers=args.n_workers)
 
     # model
     if args.model_name.startswith('hrnet'):
-        model = lib.hrnet.HRNet(args.model_name, args.pretrained, args.image_size).to('cuda')
+        model = lib.hrnet.HRNet(args.model_name, args.pretrained, args.image_size)
     elif args.model_name.startswith('vit'):
         if 'large' in args.model_name:
             embed_dim = 1024
@@ -62,7 +66,8 @@ if __name__ == '__main__':
             patch_size = 16
         elif 'patch8' in args.model_name:
             patch_size = 8
-        model = lib.vitpose.ViTPose(args.model_name, args.pretrained, args.image_size, patch_size, embed_dim).to('cuda')
+        model = lib.vitpose.ViTPose(args.model_name, args.pretrained, args.image_size, patch_size, embed_dim)
+    model.to(args.device)
 
     # optim
     optim = torch.optim.AdamW(model.parameters(), lr=args.lr)
@@ -70,16 +75,20 @@ if __name__ == '__main__':
     # train
     os.makedirs(args.log_dir, exist_ok=True)
     summary_writer = tensorboardX.SummaryWriter(args.log_dir)
+    with open(os.path.join(args.log_dir, 'args.txt'), 'w') as f:
+        json.dump(args.__dict__, f)
 
     min_val_loss = 999999
     global_step = 0
     postfix = collections.OrderedDict()
     epoch_iter = tqdm.tqdm(range(1, args.n_epochs + 1))
     for epoch_i in epoch_iter:
+        global_step += 1
         start = time.time()
+        metrics = collections.defaultdict(list)
         for batch_i, (image, target, bbox) in tqdm.tqdm(enumerate(train_dataloader), total=len(train_dataloader), leave=False):
-            image = image.to('cuda')
-            target = target.to('cuda')
+            image = image.to(args.device)
+            target = target.to(args.device)
             postfix['data_time'] = time.time() - start
             start = time.time()
 
@@ -91,52 +100,64 @@ if __name__ == '__main__':
             postfix['model_time'] = time.time() - start
             start = time.time()
 
-            metrics = {}
             with torch.no_grad():
-                prediction_landmarks = np.round(np.array(model.get_landmarks(prediction, bbox)))
-                prediction_landmarks = np.reshape(prediction_landmarks, (prediction_landmarks.shape[0], -1, 2))
-                target_landmarks = np.round(np.array(model.get_landmarks(target, bbox)))
-                target_landmarks = np.reshape(target_landmarks, (target_landmarks.shape[0], -1, 2))
-
+                pred_lands = np.round(np.array(model.get_landmarks(prediction, bbox)))
+                pred_lands = np.reshape(pred_lands, (pred_lands.shape[0], -1, 2))
+                targ_lands = np.round(np.array(model.get_landmarks(target, bbox)))
+                targ_lands = np.reshape(targ_lands, (targ_lands.shape[0], -1, 2))
                 w = np.reshape(np.array(bbox[:, 2]), (-1, 1))
-                mpjpe = evaluate.get_mpjpe(prediction_landmarks, target_landmarks, w)
-                pck = evaluate.get_pck(prediction_landmarks, target_landmarks, w)
-                aps = [evaluate.get_ap(prediction_landmarks, target_landmarks, w, eps=e)
+
+                metrics['pred_lands'].append(pred_lands)
+                metrics['targ_lands'].append(targ_lands)
+                metrics['w'].append(w)
+                metrics['loss'].append(loss.item())
+
+                mpjpe = evaluate.get_mpjpe(pred_lands, targ_lands, w)
+                pck = evaluate.get_pck(pred_lands, targ_lands, w)
+                aps = [evaluate.get_ap(pred_lands, targ_lands, w, eps=e)
                        for e in np.linspace(0.5, 0.95, 10)]
                 ap = sum(aps) / len(aps)
 
-                global_step += 1
                 postfix['train_loss'] = loss.item()
                 postfix['train_mpjpe'] = mpjpe
                 postfix['train_pck'] = pck
                 postfix['train_ap'] = ap
                 epoch_iter.set_postfix(postfix)
 
-                for metric_name, metric_value in postfix.items():
-                    if metric_name not in metrics:
-                        metrics[metric_name] = []
-                    metrics[metric_name].append(metric_value)
+        for k, v in metrics.items():
+            if k != 'loss':
+                metrics[k] = np.concatenate(v, axis=0)
+        
+        mpjpe = evaluate.get_mpjpe(metrics['pred_lands'], metrics['targ_lands'], metrics['w'])
+        pck = evaluate.get_pck(metrics['pred_lands'], metrics['targ_lands'], metrics['w'])
+        aps = [evaluate.get_ap(metrics['pred_lands'], metrics['targ_lands'], metrics['w'], eps=e)
+               for e in np.linspace(0.5, 0.95, 10)]
+        ap = sum(aps) / len(aps)
+        loss = np.mean(metrics['loss'])
 
-        metrics = {metric_name: sum(metric_values) / len(metric_values) for metric_name, metric_values in metrics}
-        for metric_name, metric_value in postfix.items():
-            postfix[metric_name] = metric_value
-            summary_writer.add_scalar(metric_name, metric_value)
+        postfix['train_mpjpe'] = mpjpe
+        postfix['train_pck'] = pck
+        postfix['train_ap'] = ap
+        postfix['train_loss'] = loss
         epoch_iter.set_postfix(postfix)
+
+        summary_writer.add_scalar('train_mpjpe', mpjpe, global_step)
+        summary_writer.add_scalar('train_pck', pck, global_step)
+        summary_writer.add_scalar('train_ap', ap, global_step)
+        summary_writer.add_scalar('train_loss', loss, global_step)
         summary_writer.flush()
 
         with torch.no_grad():
-            if epoch_i % args.f_save == 0:
-                torch.save(model.state_dict(), os.path.join(args.log_dir, f'{epoch_i}.pt'))
-
             if epoch_i % args.f_val == 0:
                 val_pred_lands = []
                 val_targ_lands = []
                 val_w = []
                 val_losses = []
+                metrics = collections.defaultdict(list)
                 for batch_i, (image, target, bbox) in tqdm.tqdm(enumerate(val_dataloader), total=len(val_dataloader),
                                                                 leave=False):
-                    image = image.to('cuda')
-                    target = target.to('cuda')
+                    image = image.to(args.device)
+                    target = target.to(args.device)
                     prediction = model(image)
                     loss = torch.nn.functional.mse_loss(prediction, target)
 
@@ -144,35 +165,44 @@ if __name__ == '__main__':
                     pred_lands = np.reshape(pred_lands, (pred_lands.shape[0], -1, 2))
                     targ_lands = np.round(np.array(model.get_landmarks(target, bbox)))
                     targ_lands = np.reshape(targ_lands, (targ_lands.shape[0], -1, 2))
+                    w = np.reshape(np.array(bbox[:, 2]), (-1, 1))
 
-                    val_pred_lands.append(pred_lands)
-                    val_targ_lands.append(targ_lands)
-                    val_w.append(np.reshape(np.array(bbox[:, 2]), (-1, 1)))
-                    val_losses.append(loss.item())
+                    metrics['pred_lands'].append(pred_lands)
+                    metrics['targ_lands'].append(targ_lands)
+                    metrics['w'].append(w)
+                    metrics['loss'].append(loss.item())
 
-                val_pred_lands = np.concatenate(val_pred_lands)
-                val_targ_lands = np.concatenate(val_targ_lands)
-                val_w = np.concatenate(val_w)
+                    epoch_iter.set_postfix(postfix)
 
-                mpjpe = evaluate.get_mpjpe(val_pred_lands, val_targ_lands, val_w)
-                pck = evaluate.get_pck(val_pred_lands, val_targ_lands, val_w)
-                aps = [evaluate.get_ap(val_pred_lands, val_targ_lands, val_w, eps=e)
-                       for e in np.linspace(0.5, 0.95, 10)]
+                for k, v in metrics.items():
+                    if k != 'loss':
+                        metrics[k] = np.concatenate(v, axis=0)
+                
+                mpjpe = evaluate.get_mpjpe(metrics['pred_lands'], metrics['targ_lands'], metrics['w'])
+                pck = evaluate.get_pck(metrics['pred_lands'], metrics['targ_lands'], metrics['w'])
+                aps = [evaluate.get_ap(metrics['pred_lands'], metrics['targ_lands'], metrics['w'], eps=e)
+                    for e in np.linspace(0.5, 0.95, 10)]
                 ap = sum(aps) / len(aps)
-                val_loss = np.mean(np.array(val_losses))
+                valid_loss = np.mean(metrics['loss'])
 
-                postfix['val_mpjpe'] = mpjpe
-                postfix['val_pck'] = pck
-                postfix['val_ap'] = ap
-                postfix['val_loss'] = val_loss
+                postfix['valid_mpjpe'] = mpjpe
+                postfix['valid_pck'] = pck
+                postfix['valid_ap'] = ap
+                postfix['valid_loss'] = valid_loss
                 epoch_iter.set_postfix(postfix)
-                summary_writer.add_scalar('val_mpjpe', postfix['val_mpjpe'], global_step)
-                summary_writer.add_scalar('val_pck', postfix['val_pck'], global_step)
-                summary_writer.add_scalar('val_ap', postfix['val_ap'], global_step)
-                summary_writer.add_scalar('val_loss', postfix['val_loss'], global_step)
-                summary_writer.flush()
 
-                if val_loss < min_val_loss:
-                    min_val_loss = val_loss
-                    torch.save(model.state_dict(), os.path.join(args.log_dir, f'best.pt'))
+                summary_writer.add_scalar('valid_mpjpe', mpjpe, global_step)
+                summary_writer.add_scalar('valid_pck', pck, global_step)
+                summary_writer.add_scalar('valid_ap', ap, global_step)
+                summary_writer.add_scalar('valid_loss', valid_loss, global_step)
+                summary_writer.flush()
+            
+            torch.save(model.state_dict(), os.path.join(args.log_dir, f'last.pt'))
+
+            if valid_loss < min_val_loss:
+                min_val_loss = valid_loss
+                torch.save(model.state_dict(), os.path.join(args.log_dir, f'best.pt'))
+
+            if epoch_i % args.f_save == 0:
+                torch.save(model.state_dict(), os.path.join(args.log_dir, f'{epoch_i}.pt'))
 
